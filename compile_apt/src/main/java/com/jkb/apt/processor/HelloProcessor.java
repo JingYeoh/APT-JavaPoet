c9bc2e0950cbe890808ae6079a5d8b4b35d6f474
JustKiddingBaby <yangjing9611@foxmail.com>
2017-11-14 22:27:29 +0800

    添加google-auto框架，复写处理器两种方法;

--- compile_apt/src/main/java/com/jkb/apt/processor/HelloProcessor.java
+++ compile_apt/src/main/java/com/jkb/apt/processor/HelloProcessor.java
@@ -1,22 +1,28 @@
 package com.jkb.apt.processor;
 
 
+import com.google.auto.service.AutoService;
+import com.jkb.apt.annimator.HelloAPT;
+
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 /**
- * please input description for this class.
+ * APT注解解释器
  *
  * @author JingYeoh.
  *         Date 17-11-14.
  *         Github https://github.com/justkiddingbaby
  *         Blog http://blog.justkiddingbaby.com
  */
+@AutoService(Processor.class)
 public class HelloProcessor extends AbstractProcessor {
 
     @Override
@@ -31,11 +37,13 @@ public class HelloProcessor extends AbstractProcessor {
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        return super.getSupportedAnnotationTypes();
+        Set<String> result = new HashSet<>();
+        result.add(HelloAPT.class.getName());
+        return result;
     }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
-        return super.getSupportedSourceVersion();
+        return SourceVersion.latestSupported();
     }
 }
